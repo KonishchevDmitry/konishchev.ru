7f2d061441d6d855bd19371796a9b42693c67e27
Dmitry Konishchev <konishchev@gmail.com>
2014-02-25 15:38:46 +0400

    Fix broken develop mode

--- homepage/main.py
+++ homepage/main.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-import homepage.views;
-
-from homepage import app
-
 if __name__ == "__main__":
     _package_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     if _package_path not in sys.path:
         sys.path.insert(0, _package_path)
 
-    homepage.app.run(host="::1", debug=True)
+import homepage.views
+
+from homepage import app
+
+if __name__ == "__main__":
+    app.run(host="::1", debug=True)
